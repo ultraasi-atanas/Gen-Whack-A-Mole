96dcc622f5ffbf9ec65959125ed0343b6ff0005f
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2021-04-21 19:53:53 +0100

    Create grid 4x4

---
+++ script.js
@@ -0,0 +1,15 @@
+// same as getElementsByClassName
+let allTiles = document.querySelectorAll(".oneTile"); 
+allTiles.forEach( oneTile => {oneTile.appendChild})
+
+// when a cell is clicked - pass the ID of the cell to a function
+function clickedBox(boxId) {
+
+}
+
+// randomly make a mole appear
+function displayMole () {
+
+// getElementById - get random ones, when you pick it
+
+}
