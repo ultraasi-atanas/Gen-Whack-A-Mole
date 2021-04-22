2343d389b47a112daf389919ffb51e249edea88f
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2021-04-22 18:58:39 +0100

    Assign Tile Div ids programmatically

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// same as getElementsByClassName
+// HERE WE SET THE ATTRIBUTE ID TO ALL TILES
+// we grab all tile divs, tagged by the same class name oneTile
 let allTiles = document.querySelectorAll(".oneTile"); 
-allTiles.forEach( oneTile => {oneTile.appendChild})
+let idVariable = 1;
+allTiles.forEach( oneTile => {
+    // oneTile.setAttribute("id", "id_you_like")
+    oneTile.setAttribute("id", `t${idVariable}`)
+    // increase idVariable
+    idVariable += 1
+}
+)
 
 // when a cell is clicked - pass the ID of the cell to a function
 function clickedBox(boxId) {
