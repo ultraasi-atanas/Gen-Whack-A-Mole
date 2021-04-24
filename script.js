5db7c38911b84c91758fd227757bd47a1fe355cc
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2021-04-24 07:07:09 +0100

    Add id and innerHTML to tile programmatically with JS

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+"use strict";
+
+
+//START
+//
+//
 // HERE WE SET THE ATTRIBUTE ID TO ALL TILES
 // we grab all tile divs, tagged by the same class name oneTile
 let allTiles = document.querySelectorAll(".oneTile"); 
+
 let idVariable = 1;
+
 allTiles.forEach( oneTile => {
     // oneTile.setAttribute("id", "id_you_like")
     oneTile.setAttribute("id", `t${idVariable}`)
-    // increase idVariable
+    // Change text inside that tile, e.g. Tile 1
+    oneTile.innerHTML = `${idVariable}`;
+    // Create an event listener / event handler 
+    // increase idVariable, so that next time has a higher number
     idVariable += 1
 }
 )
